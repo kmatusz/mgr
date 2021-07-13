c692496ca8ba5a19d4ff31e7e3835f76389a5933
kmatusz <kmatuszelanski@gmail.com>
2021-07-13 20:20:38 +0200

    improved plots in results part

--- thesis/makefile.R
+++ thesis/makefile.R
@@ -3,9 +3,12 @@ library(rmarkdown)
 include_code <- FALSE
 
 
-rmarkdown::render("thesis/00_thesis.Rmd",output_dir = 'thesis/versions/', output_file = 'thesis_init.docx', params = list(echo=include_code))
+rmarkdown::render("thesis/00_thesis.Rmd",
+                  output_dir = 'thesis/versions/', 
+                  output_file = '13_07_v0_thesis.docx', 
+                  params = list(echo=include_code))
 
-x# HTML
+# HTML
 # rmarkdown::render("thesis/01_introduction.Rmd", output_format = "html_document", params = list(echo=include_code))
 rmarkdown::render("thesis/02_literature_review.Rmd", output_format = "html_document", params = list(echo=include_code))
 rmarkdown::render("thesis/03_dataset_description.Rmd", output_format = "html_document", params = list(echo=include_code))
--- thesis/sketchbook.R
+++ thesis/sketchbook.R
@@ -1,3 +1,23 @@
+var_imp_all <- varImp(models_list$all_with_pca$model, scale=F)
+
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+rownames(var_imp_all$importance)
+
+var_imp_all$importance %>%
+  as_tibble(rownames = 'var') %>%
+  mutate(var2 = case_when(
+    str_starts(var, 'prod_cat_') ~ 'product_categories',
+    str_starts(var, 'spatial_') ~ 'geodemographic',
+    str_starts(var, 'topic_') ~ 'review_topic',
+    str_starts(var, 'agglomeration') ~ 'agglomeration',
+    TRUE ~ 'basic')) %>%
+  group_by(var2) %>%
+  summarise(Overall = sum(Overall)) %>%
+  mutate(Overall = range01(Overall)*100) %>%
+  as.data.frame() -> temp_df_imp
+
+
 temp_df_imp %>%
   ggplot(aes(x = var2, y = Overall, label = round(Overall))) +
   geom_point(stat='identity', fill="black", size=7) +
@@ -14,7 +34,52 @@ temp_df_imp %>%
     x = 'Variable',
     y = 'Importance'
   )
-  
+
+
+
+###################
+
+var_imp_all <- varImp(models_list$all_with_pca$model, scale=F)
+
+range01 <- function(x){(x-min(x))/(max(x)-min(x))}
+
+
+var_imp_all$importance %>%
+  as_tibble(rownames = 'var') %>%
+  mutate(var2 = case_when(
+    
+    str_starts(var, 'prod_cat_') ~ 'prod_categories',
+    str_starts(var, 'spatial_') ~ 'geodemographic',
+    str_starts(var, 'topic_') ~ 'review_topic',
+    str_starts(var, 'review_') ~ 'perception',
+    str_starts(var, 'geolocation_') ~ 'geolocation',
+    var %in% c('payment_value', 'sum_freight', 'no_items', 'prod_categories') ~ 'behavioural',
+    TRUE ~ var
+  )) %>%
+  group_by(var2) %>%
+  summarise(Overall = sum(Overall)) %>%
+  mutate(Overall = range01(Overall)*100) %>%
+  as.data.frame() -> temp_df_imp
+
+
+temp_df_imp %>%
+  ggplot(aes(x = var2, y = Overall, label = round(Overall))) +
+  geom_point(stat='identity', fill="black", size=7) +
+  geom_segment(aes(y = 0,
+                   x = var2,
+                   yend = Overall,
+                   xend = var2),
+               color = "black") +
+  geom_text(color="white", size=3) +
+  # ylim(-2.5, 2.5) +
+  coord_flip() +
+  theme_minimal() +
+  labs(
+    x = 'Variable',
+    y = 'Importance'
+  )
+
+
 
 ### DUMP ----
 
